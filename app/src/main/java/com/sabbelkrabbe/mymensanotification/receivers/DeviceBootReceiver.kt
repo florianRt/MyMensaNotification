9095e9827c1b244a3d013338d1b97afdb59d3435
Sabbelkrabbe <rathflorian8@gmail.com>
2023-07-24 17:02:44 +0200

    fixed update of widget when rebooting device

--- app/src/main/java/com/sabbelkrabbe/mymensanotification/receivers/DeviceBootReceiver.kt
+++ app/src/main/java/com/sabbelkrabbe/mymensanotification/receivers/DeviceBootReceiver.kt
@@ -1,27 +1,33 @@
 package com.sabbelkrabbe.mymensanotification.receivers
 
+import android.appwidget.AppWidgetManager
 import android.content.BroadcastReceiver
-import android.content.Intent
-import com.sabbelkrabbe.mymensanotification.receivers.AlarmReceiver
-import android.app.PendingIntent
-import android.app.AlarmManager
+import android.content.ComponentName
 import android.content.Context
-import java.util.*
+import android.content.Intent
+import com.sabbelkrabbe.mymensanotification.MenuOverview
+
 
 class DeviceBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-//        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
-//            // on device boot complete, reset the alarm
-//            val alarmIntent = Intent(context, AlarmReceiver::class.java)
-//            val pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0)
-//            val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//            val calendar = Calendar.getInstance()
-//            calendar.timeInMillis = System.currentTimeMillis()
-//            calendar[Calendar.HOUR_OF_DAY] = 7
-//            calendar[Calendar.MINUTE] = 0
-//            calendar[Calendar.SECOND] = 1
-//            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis,
-//                AlarmManager.INTERVAL_DAY, pendingIntent)
-//        }
+
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            updateWidget(context)
+        }
+    }
+
+    private fun updateWidget(context: Context) {
+        val ids = AppWidgetManager.getInstance(context).getAppWidgetIds(
+            ComponentName(
+                context.applicationContext,
+                MenuOverview::class.java
+            )
+        )
+        val myWidget = MenuOverview()
+        myWidget.onUpdate(
+            context.applicationContext,
+            AppWidgetManager.getInstance(context.applicationContext),
+            ids
+        )
     }
 }
